901b6003666a9854b357e43615f42416b28bc4dd
vijayamounika <vjmnkvld8@gmail.com>
2018-09-24 19:38:07 +0530

    getting daily subscriptions one per day

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ MongoClient.connect(url, function (err, client) {
         console.log('Connected to MongoDB');
         db.collection('newuser').createIndex({ email: 1 }, { unique: true });
         db.collection('newSubscription').createIndex({ userid: 1, name: 1 }, { unique: true });
+        //db.collection('dailySubscriptions').createIndex({ name: 1, subId: 1 }, { unique: true });
 
     }
 });
@@ -44,6 +45,7 @@ app.post('/auth/user/addNewSubscription', function (req, res, next) {
     var pricePerUnit = req.body.pricePerUnit;
     var quantity = req.body.quantity;
     var unit = req.body.unit;
+    var subtype = req.body.subtype;
     var user_id = req.user._id;
 
     if (unitArray.includes(unit)) {
@@ -52,6 +54,7 @@ app.post('/auth/user/addNewSubscription', function (req, res, next) {
             pricePerUnit: pricePerUnit,
             quantity: quantity,
             unit: unit,
+            subType: subtype,
             userid: user_id
         };
         //removed try catch block and commented out user finding logic as we are verifying token and getting user from middleware function
@@ -165,40 +168,95 @@ function validateSubscriptionId(req, res, next) {
     db.collection('newSubscription').findOne({ "userid": req.user._id }, { "_id": mongo.ObjectId(subId) }, function (err, result) {
         if (err) throw new Error("error");
         if (result) {
+            req.subscription = result;
             next();
         }
         else throw new Error("There is no subscription with the requested id");
     })
 }
+function getSpecificDate(req, res, next) {
+    var subId = req.body.subscriptionId;
+    var date1 = new Date(req.body.date);
+    var givenDate = date1.getDate() + 1;
+    var givenMonth = date1.getMonth() + 1;
+    var givenYear = date1.getFullYear();
+    var str1 = req.body.date;
+    if (givenMonth < 10) {
+        givenMonth = "0" + givenMonth;
+    }
+    var str = str = givenYear + "-" + givenMonth + "-" + givenDate;
+    var date2 = new Date(str);
 
+    db.collection('dailySubscriptions').find({ "subId": subId, "date": { "$gte": new Date(str1), "$lt": new Date(str) } }).toArray(function (err, result) {
+        if (err) throw new Error("Error");
+        if (result) {
+            req.recordOnDate = result;
+            next();
+        }
+        else throw new Error("There is no subscription tracking for the requested one");
+    })
+}
 app.post('/auth/user/dailySubscriptionTrack', function (req, res) {
     var subId = req.body.subscriptionId;
     var prName = req.body.name;
 
-    db.collection('newSubscription').findOne({ "_id": mongo.ObjectID(subId) }, { "name": prName }, function (err, docs) {
+    db.collection('newSubscription').findOne({ "_id": mongo.ObjectID(subId), "name": prName }, function (err, docs) {
         if (err) throw new Error("Error occurred");
         if (docs) {
-
             var quantity = req.body.quantity;
-            var cost = docs.pricePerUnit * quantity;
-            //var today = new Date();
+            if (docs.subType == "daily") {
+                if (req.body.priceperunit) {
+                    cost = req.body.priceperunit * quantity;
+                } else {
+                    throw new Error("please enter price per unit");
+                }
 
+            } else cost = docs.pricePerUnit * quantity;
+
+            var day = new Date().getDate();
+            var month = new Date().getMonth() + 1;
+            if (month < 10) {
+                month = "0" + month;
+            }
+            var year = new Date().getFullYear();
+            dateString = 2018 + "-" + "0" + 9 + "-" + 26 + "T00:00:00.000Z";
+            var mydate = new Date(dateString);
             myDailySubscription = {
                 name: prName,
-                Date: new Date(),
+                date: mydate,
                 quantity: quantity,
                 cost: cost,
                 subId: subId
-            }
-            db.collection('dailySubscriptions').insert(myDailySubscription, function (err, result) {
-                if (err) throw new Error("error");
-                if (result) {
-                    res.send("successfully inserted");
+            };
+            db.collection('dailySubscriptions').find({ "$and": [{ "$expr": { "$eq": ["$date", mydate] } }, { "name": prName }] }).toArray(function (err, result) {
+                // db.collection('dailySubscriptions').find({ "name": prName, "date": day }).toArray(function (err, result) {
+                if (err) throw new Error("Error");
+                if (result.length > 0) res.send("Cannot create two daily subscriptions on a single date, please update to change");
+                else {
+                    db.collection('dailySubscriptions').insertOne(myDailySubscription, function (err, result) {
+                        if (err) throw new Error("error");
+                        if (result) {
+                            res.send("successfully inserted");
+                        }
+                        else res.send("Error occurred while tracking");
+                    })
                 }
-                else res.send("Error occurred while tracking")
             })
         }
     })
+})
+app.post('/auth/user/getSpecificDateSubscriptionAndUpdate', validateSubscriptionId, getSpecificDate, function (req, res) {
+    var quantity = req.body.quantity;
+    db.collection('dailySubscriptions').update({ "_id": req.recordOnDate[0]._id }, { $set: { "quantity": quantity } }, function (err, result) {
+        if (err) throw new Error("error");
+        if (result)
+            res.send("updated successfully");
+        else throw new Error("error");
+    });
+})
+app.post('/auth/user/getSpecificDateSubscriptionAndDelete', validateSubscriptionId, getSpecificDate, function (req, res) {
+    db.collection('dailySubscriptions').remove({ "_id": req.recordOnDate[0]._id });
+    res.send("Deleted successfully");
 })
 app.post('/auth/user/getTillDateSubscriptionCost', validateSubscriptionId, function (req, res) {
     var subId = req.body.subscriptionId;
@@ -222,8 +280,15 @@ app.post('/auth/user/getTillDateSubscriptionCost', validateSubscriptionId, funct
         }
         else res.send("Error");
     })
-
 })
+app.post('/auth/user/getSpecifiedDateSubscriptions', validateSubscriptionId, getSpecificDate, function (req, res) {
+    if (req.user) {
+        var myJson1 = JSON.stringify(req.user);
+        res.send(myJson1);
+    }
+    else throw new Error("Error");
+})
+
 app.get('/', (req, res) => res.send("Hello world"));
 app.get('/home', (req, res) => res.json({ message: "hurray, you did it" }));
 app.get('/login', function (req, res) {
@@ -240,3 +305,120 @@ app.get('/users/:username/:password', function (req, res) {
     res.send(req.params.username);
 });
 app.listen(3000, () => console.log('Example is listening on port 3000'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
